61fbf6dd268e659450bedcdc1c0e14b6a05e0181
Ole Faltinsen <ole@adsign.no>
2016-12-07 21:39:40 +0100

    build.... shoud automate this

--- dist/main.js
+++ dist/main.js
@@ -4,18 +4,20 @@ var _lunchbot = require('./lib/lunchbot');
 
 var _lunchbot2 = _interopRequireDefault(_lunchbot);
 
-var _config = require('../config');
-
-var _config2 = _interopRequireDefault(_config);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var token = process.env.slack_token || _config2.default.slack_token;
-var name = process.env.bot_name || _config2.default.bot_name;
+if (process.env) {
+  var _token = process.env.slack_token;
+  var _name = process.env.bot_name;
+} else {
+  var CONFIG = require('../config');
+  var _token2 = CONFIG.slack_token;
+  var _name2 = CONFIG.bot_name;
+}
 
 var lunchbot = new _lunchbot2.default({
-    token: token,
-    name: name
+  token: token,
+  name: name
 });
 
 lunchbot.run();
