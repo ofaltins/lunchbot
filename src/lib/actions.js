19b25159c083e9b82510507e49ac7c9301899ac8
Ole Faltinsen <ole@adsign.no>
2016-12-08 13:53:58 +0100

    moved actions to separate file, implemented eventemitter

---
+++ src/lib/actions.js
@@ -0,0 +1,165 @@
+import Julebygda from '../api/julebygda';
+const CONFIG = process.env.NODE_ENV === 'production' ? process.env : require('../../config')
+const username = CONFIG.julebygda_user
+const password = CONFIG.julebygda_password
+const julebygda = new Julebygda(username, password);
+
+class Actions {
+  constructor (emitter) {
+    this.eventEmitter = emitter
+    this.state = {}
+  }
+  setState (state) {
+    this.state = state
+  }
+  actions() {
+    return {
+      finn: {
+        public: true,
+        doc: 'Hjelper deg med å finne de rette varene, og å legge varene inn i handlelisten.',
+        func: (origin, term) => { // expects @string
+          this.eventEmitter.emit('say', 'Søker - vent litt!', origin)
+          julebygda.search(term).then(results => {
+            if (results.length > 19) {
+              this.eventEmitter.emit('say', 'Oi der fant jeg mange varer. Kanskje du skal prøve å søke mer spesifikt? Jeg kommer tilbake med de første 20 resultatene straks.', origin)
+              results = results.slice(0,19)
+            }
+            return julebygda.getProductData(results)
+          })
+          .then(r => {
+            let output = "Her er det jeg fant:\nVarenummer\t\t\tNavn\t\t\tPris\n"
+            if (r.length > 0) {
+              r.forEach((item) => {
+                output += item.id + "\t\t\t" + item.tittel + "\t\t\t" + item.price + "\n"
+              })
+            output += "\nFor å legge til en vare i handlelisten: 'lunchbot kjøp [varenummer]'"
+            } else {
+              output += 'INGENTING!'
+            }
+            this.eventEmitter.emit('say', output, origin)
+          })
+        }
+      },
+      kjøp: {
+        public: true,
+        doc: 'Når du har funnet en vare vha. finn-kommandoen, kan du bruke kjøp for å velge hvilken vare fra søkeresultatet som skal legges i handlelisten. Du kan legge inn flere varenummer på en gang. F.eks: lunchbot kjøp 205310 116744 108656',
+        func: (origin, items) => { // expects space-separated @string with item ids
+          const getItems = items.split(' ').map(item => { return {id: item}})
+          julebygda.getProductData(getItems)
+            .then(productData => {
+              const shoppingList = julebygda.addToShoppingList(productData)
+              let output = "Handlelisten er oppdatert\n"
+              shoppingList.forEach(item => {
+                output += item.id + "\t" + item.tittel + "\t" + item.price + "\n"
+              })
+              this.eventEmitter.emit('say', output, origin)
+            })
+        }
+      },
+      vishandleliste: {
+        public: true,
+        doc: 'Viser handlelisten. Alle kan legge til varer i handlelisten, men det er kun lunsjansvarlig som kan fjerne',
+        func: origin => {
+          const shoppingList = julebygda.getShoppingList()
+          let output = shoppingList.length > 0 ? "Her er handlelisten\n" : "Handlelisten er tom!"
+          shoppingList.forEach(item => {
+            output += item.id + "\t" + item.tittel + "\t" + item.price + "\n"
+          })
+          this.eventEmitter.emit('say', output, origin)
+        }
+      },
+      nyhandleliste: {
+        public: true,
+        doc: 'Kaster den gamle handlelisten, og lager en ny blank handleliste',
+        func: origin => {
+          julebygda.clearShoppingList()
+          this.eventEmitter.emit('say', 'Handlelisten er tømt', origin)
+        }
+      },
+      vishandlekurv: {
+        public: false,
+        doc: 'Viser det som er lagret i sesjonen hos julebygda.no.',
+        func: origin => {
+          julebygda.viewBasket()
+            .then(basket => {
+              let output = "Dette ligger nå i handlekurven\n"
+              basket.varer.forEach(item => {
+                output += item + "\n"
+              })
+              output += "\n" + basket.oppsummering
+              this.eventEmitter.emit('say', output, origin)
+            })
+        }
+      },
+      sendbestilling: {
+        public: true,
+        doc: 'Lager en ny ordre hos julebygda.no med alt som ligger i handlelisten, og viser ordresammendrag før du kan bekrefte bestilling',
+        func: origin => {
+          julebygda.addToCart(julebygda.getShoppingList())
+            .then(response => {
+              // this.eventEmitter.emit('say', response)
+              return julebygda.viewBasket()
+            }).then(basket => {
+              let output = "Dette ligger nå i handlekurven\n"
+              basket.varer.forEach(item => {
+                output += item.tittel + "\t" + item.price + "\t" + item.amount + "\t" + item.subtotal + "\n"
+              })
+              output += "\n" + basket.oppsummering
+              output += "\n\nBekreft bestilling med 'lunchbot bekreftbestilling'"
+              this.eventEmitter.emit('say', output, origin)
+              // send order here
+            }).catch(error => {
+              this.eventEmitter.emit('say', error, origin)
+            })
+        }
+      },
+      bekreftbestilling: {
+        public: true,
+        doc: 'For bruk etter at du har brukt sendbestilling. Denne kommandoen effektuerer bestillingen hos julebygda.no',
+        func: origin => {
+          julebygda.viewBasket()
+            .then(basket => {
+              return julebygda.confirmOrder(basket.formData)
+            })
+            .then(result => {
+              this.eventEmitter.emit('say', 'Maybe it worked?', origin)
+            })
+            .catch(error => {
+              console.log('confirmorder error', error)
+              this.eventEmitter.emit('say', error, origin)
+            })
+        }
+      },
+      hvemersjef: {
+        public: true,
+        doc: 'Forteller hvem som har lunchansvar (og dermed min gud) denne uken',
+        func: origin => {
+          console.log('STATE', this.state)
+          this.eventEmitter.emit('say', this.state.admin + ' er så sykt sjef', origin)
+        }
+      },
+      nysjef: {
+        public: true,
+        restricted: true,
+        doc: 'O lunch med din glede! Sett en ny lunchansvarlig slik: nysjef navn-på-stakkars-jævel',
+        func: (origin, user) => {
+          this.eventEmitter.emit('setadmin', user)
+          this.eventEmitter.emit('say', 'Ny sjef er ' + user, origin)
+        }
+      },
+      hjelp: {
+        public: true,
+        doc: 'Denne menyen! Duh!',
+        func: origin => {
+          let output = 'Her er det jeg kan hjelpe deg med:' + "\n\n"
+          for (let action in this.actions()) {
+            output += '*' + action + "*\n" + this.actions()[action].doc + "\n\n"
+          }
+          this.eventEmitter.emit('say', output, origin)
+        }
+      }
+    }
+  }
+}
+
+export default Actions
--- src/lib/lunchbot.js
+++ src/lib/lunchbot.js
@@ -1,20 +1,37 @@
-import Julebygda from '../api/julebygda';
 import Bot from 'slackbots';
+import Actions from './actions'
+import Events from 'events'
 
-const CONFIG = process.env.NODE_ENV === 'production' ? process.env : require('../../config')
-const username = CONFIG.julebygda_user
-const password = CONFIG.julebygda_password
+class EventEmitter extends Events {}
+const eventEmitter = new EventEmitter()
 
-const julebygda = new Julebygda(username, password);
+const ACTIONS = new Actions(eventEmitter)
 
 class Lunchbot extends Bot {
   constructor (settings) {
     super (settings);
+
+    ACTIONS.setState(settings)
     this.settings = settings;
     this.settings.activeChannels = [];
 
-    // TODO: set db path
+    eventEmitter.on('say', (payload, origin) => {
+      console.log('got event', payload, origin)
+      if (this._isChannelConversation(origin)) {
+        this._postMessageToChannel(payload, origin)
+      } else if (this._isDirectMessage(origin)) {
+        this._postMessageToUser(payload, origin)
+      } else {
+        console.log('Payload without target: ', payload, origin)
+      }
+    })
+
+    eventEmitter.on('setadmin', user => {
+      this.settings.admin = user
+      ACTIONS.setState(this.settings)
+    })
   }
+
   run () {
     this.on('start', this._onStart)
     this.on('message', this._onMessage)
@@ -30,11 +47,9 @@ class Lunchbot extends Bot {
       this.settings.activeChannels.push(channel.name)
       console.log('bot is in: ' + channel.name)
     });
-
   }
   _onMessage (message) {
     if (this._isChatMessage(message)
-        && this._isChannelConversation(message)
         && !this._isFromMe(message)
         && this._isAdressingMe(message)
     ) {
@@ -55,7 +70,10 @@ class Lunchbot extends Bot {
     return message.type === 'message' && Boolean(message.text)
   }
   _isChannelConversation (message) {
-    return typeof message.channel === 'string' && message.channel[0] === 'C'
+    return typeof message.channel === 'string' && message.channel[0] === 'C' || message.channel[0] === 'G'
+  }
+  _isDirectMessage (message) {
+    return typeof message.channel === 'string' && message.channel[0] === 'D'
   }
   _isAdressingMe (message) {
     const first = message.text.toLowerCase().split(' ')[0]
@@ -76,13 +94,17 @@ class Lunchbot extends Bot {
     const username = this._getUserName(message.user)
     return username === this.settings.admin || this._isBotAdmin(message.user) === true
   }
-  _getChannelById (channelId) {
-    return this.channels.filter((item) => {
-      return item.id === channelId
-    })[0];
+  _getChannelNameById (channelId) {
+    const channel = this.channels.filter(item => item.id === channelId)[0] || this.groups.filter(item => item.id === channelId)[0]
+    return channel === undefined ? false : channel.name
   }
-  _postMessageToChannel (message) {
-    return this.postMessageToChannel('lunch', message, {as_user: true})
+  _postMessageToChannel (message, origin) {
+    const channel = origin === undefined ? this.settings.primary_channel : this._getChannelNameById(origin.channel)
+    return this.postTo(channel, message, {as_user: true})
+  }
+  _postMessageToUser (message, origin) {
+    const username = this._getUserName(origin.user)
+    return this.postMessageToUser(username, message)
   }
   _parseCommand(message) {
     let input = message.text.toLowerCase().split(' ')
@@ -94,163 +116,19 @@ class Lunchbot extends Bot {
     console.log('command:' + command + ' arg: ' + argument)
 
     // if command matches a function name in the _actions method, run it and pass one argument
-    const action = this._actions()[command];
-    if (action !== undefined) { 
-      action.func(argument, message)
+    const action = ACTIONS.actions()[command];
+    if (action !== undefined) {
+      if ((action.restricted === true && this._isFromAdmin(message)) || action.restricted !== true) {
+        action.func(message, argument)
+      } else {
+        this._postMessageToChannel('Du har ikke tilgang til denne funksjonen.')
+      }
     } else {
       this._postMessageToChannel('Øyh! Det skjønte jeg ikke bæret av. Si noe jeg forstår da? For å se alt du kan spørre meg om, skriv lunchbot hjelp')
     }
   }
   _actions() {
-    const self = this;
-    return {
-      finn: {
-        public: true,
-        doc: 'Hjelper deg med å finne de rette varene, og å legge varene inn i handlelisten.',
-        func: term => { // expects @string
-          self._postMessageToChannel('Søker - vent litt!')
-          julebygda.search(term).then(results => {
-            if (results.length > 19) {
-              self._postMessageToChannel('Oi der fant jeg mange varer. Kanskje du skal prøve å søke mer spesifikt? Jeg kommer tilbake med de første 20 resultatene straks.')
-              results = results.slice(0,19)
-            }
-            return julebygda.getProductData(results)
-          })
-          .then(r => {
-            let output = "Her er det jeg fant:\nVarenummer\t\t\tNavn\t\t\tPris\n"
-            if (r.length > 0) {
-              r.forEach((item) => {
-                output += item.id + "\t\t\t" + item.tittel + "\t\t\t" + item.price + "\n"
-              })
-            output += "\nFor å legge til en vare i handlelisten: 'lunchbot kjøp [varenummer]'"
-            } else {
-              output += 'INGENTING!'
-            }
-            self._postMessageToChannel(output)
-          })
-        }
-      },
-      kjøp: {
-        public: true,
-        doc: 'Når du har funnet en vare vha. finn-kommandoen, kan du bruke kjøp for å velge hvilken vare fra søkeresultatet som skal legges i handlelisten. Du kan legge inn flere varenummer på en gang. F.eks: lunchbot kjøp 205310 116744 108656',
-        func: items => { // expects space-separated @string with item ids
-          const getItems = items.split(' ').map(item => { return {id: item}})
-          julebygda.getProductData(getItems)
-            .then(productData => {
-              const shoppingList = julebygda.addToShoppingList(productData)
-              let output = "Handlelisten er oppdatert\n"
-              shoppingList.forEach(item => {
-                output += item.id + "\t" + item.tittel + "\t" + item.price + "\n"
-              })
-              self._postMessageToChannel(output)
-            })
-        }
-      },
-      vishandleliste: {
-        public: true,
-        doc: 'Viser handlelisten. Alle kan legge til varer i handlelisten, men det er kun lunsjansvarlig som kan fjerne',
-        func: () => {
-          const shoppingList = julebygda.getShoppingList()
-          let output = shoppingList.length > 0 ? "Her er handlelisten\n" : "Handlelisten er tom!"
-          shoppingList.forEach(item => {
-            output += item.id + "\t" + item.tittel + "\t" + item.price + "\n"
-          })
-          self._postMessageToChannel(output)
-        }
-      },
-      nyhandleliste: {
-        public: true,
-        doc: 'Kaster den gamle handlelisten, og lager en ny blank handleliste',
-        func: () => {
-          julebygda.clearShoppingList()
-          self._postMessageToChannel('Handlelisten er tømt')
-        }
-      },
-      vishandlekurv: {
-        public: false,
-        doc: 'Viser det som er lagret i sesjonen hos julebygda.no.',
-        func: () => {
-          julebygda.viewBasket()
-            .then(basket => {
-              let output = "Dette ligger nå i handlekurven\n"
-              basket.varer.forEach(item => {
-                output += item + "\n"
-              })
-              output += "\n" + basket.oppsummering
-              self._postMessageToChannel(output)
-            })
-        }
-      },
-      sendbestilling: {
-        public: true,
-        doc: 'Lager en ny ordre hos julebygda.no med alt som ligger i handlelisten, og viser ordresammendrag før du kan bekrefte bestilling',
-        func: () => {
-          julebygda.addToCart(julebygda.getShoppingList())
-            .then(response => {
-              // self._postMessageToChannel(response)
-              return julebygda.viewBasket()
-            }).then(basket => {
-              let output = "Dette ligger nå i handlekurven\n"
-              basket.varer.forEach(item => {
-                output += item.tittel + "\t" + item.price + "\t" + item.amount + "\t" + item.subtotal + "\n"
-              })
-              output += "\n" + basket.oppsummering
-              output += "\n\nBekreft bestilling med 'lunchbot bekreftbestilling'"
-              self._postMessageToChannel(output)
-              // send order here
-            }).catch(error => {
-              self._postMessageToChannel(error)
-            })
-        }
-      },
-      bekreftbestilling: {
-        public: true,
-        doc: 'For bruk etter at du har brukt sendbestilling. Denne kommandoen effektuerer bestillingen hos julebygda.no',
-        func: () => {
-          julebygda.viewBasket()
-            .then(basket => {
-              return julebygda.confirmOrder(basket.formData)
-            })
-            .then(result => {
-              self._postMessageToChannel('Maybe it worked?')
-            })
-            .catch(error => {
-              console.log('confirmorder error', error)
-              self._postMessageToChannel(error)
-            })
-        }
-      },
-      hvemersjef: {
-        public: true,
-        doc: 'Forteller hvem som har lunchansvar (og dermed min gud) denne uken',
-        func: () => {
-          self._postMessageToChannel(this.settings.admin + ' er så sykt sjef')
-        }
-      },
-      nysjef: {
-        public: true,
-        doc: 'O lunch med din glede! Sett en ny lunchansvarlig slik: nysjef navn-på-stakkars-jævel',
-        func: (user, message) => {
-          if (this._isFromAdmin(message)) {
-            this.settings.admin = user
-            self._postMessageToChannel('Ny sjef er ' + user)
-          } else {
-            self._postMessageToChannel('Kun en sjef kan bestemme ny sjef!')
-          }
-        }
-      },
-      hjelp: {
-        public: true,
-        doc: 'Denne menyen! Duh!',
-        func: () => {
-          let output = 'Her er det jeg kan hjelpe deg med:' + "\n\n"
-          for (let action in this._actions()) {
-            output += '*' + action + "*\n" + this._actions()[action].doc + "\n\n"
-          }
-          self._postMessageToChannel(output)
-        }
-      }
-    }
+    return new Actions()
   }
 }
 
